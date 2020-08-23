12c41cd50f016f2cc0e3d51c5ec3bc04f7645c18
adiirwanto <irwantoadi926@gmail.com>
2020-08-23 10:26:29 +0700

    memperbaiki tampilan login

--- app/src/main/java/com/irwantostudio/wonderfuljateng/LoginActivity.java
+++ app/src/main/java/com/irwantostudio/wonderfuljateng/LoginActivity.java
@@ -91,4 +91,8 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void onClick(View view) {
+        startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+    }
 }
